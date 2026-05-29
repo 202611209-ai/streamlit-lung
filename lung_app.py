23c3dc1074d81cf2eff10ac55e49aaba8914dd6b
202611209-ai <2026_11209@kshcm.net>
2026-05-29 13:16:27 +0700

    Update lung_app.py

--- lung_app.py
+++ lung_app.py
@@ -15,7 +15,7 @@ st.set_page_config(page_title="폐암 환자 군집 분석", layout="wide")
 st.title("🫁 폐암 환자 데이터 군집 분석 및 새로운 환자 위치 시각화")
 st.write("왼쪽 사이드바에서 환자의 정보를 입력하면, 군집 분석 결과 그래프에 환자의 위치가 실시간으로 표시됩니다.")
 font_name = "MalgunGothic.ttf" 
-
+import os
 if os.path.exists(font_name):
     # 깃허브 서버(리눅스) 환경일 때: 내 폴더의 폰트 파일을 직접 등록
     fm.fontManager.addfont(font_name)
